a2111f9e33c75f58c01f8cf8e3df477025590689
Paul Aglipay <PAglipay@ad.it.ucla.edu>
2022-03-15 11:12:20 -0700

    change to /test route

--- demo/src/main/java/info/paglipay/springdemo/controller/TestController.java
+++ demo/src/main/java/info/paglipay/springdemo/controller/TestController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
 	@GetMapping("/test")
 	public String test() {
-		return "test works";
+		return "test works change here";
 	}
 	
 }
