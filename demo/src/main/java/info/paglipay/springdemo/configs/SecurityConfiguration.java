485de090e126b8e830d6c6c6a08794c1dab81944
Paul Aglipay <paglipay@gmail.com>
2022-03-15 17:30:16 -0700

    added models and working default security login

---
+++ demo/src/main/java/info/paglipay/springdemo/configs/SecurityConfiguration.java
@@ -0,0 +1,27 @@
+package info.paglipay.springdemo.configs;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+//import info.paglipay.springdemo.services.AuthService;
+
+@Configurable
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+//  @Autowired
+//  private AuthService authService;
+	
+	  @Override
+	  protected void configure(HttpSecurity http) throws Exception {
+	    // Enable CORS and disable CSRF
+	    http.csrf().disable();
+//	    http.sessionManagement().sessionCreationPolicy('STATELESS');
+
+	    // Set session management to stateless
+	    http.authorizeRequests().anyRequest().permitAll();
+	  }
+
+}
